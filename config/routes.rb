2d72f447924f98ce5d6b7604aec11161d43ea721
Hugo Daniel <hugodaniel.icam@gmail.com>
2020-08-12 09:39:48 +0200

    Collection and member routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+
+  # get "/restaurants/top", to: "restaurants#top", as: :top_restaurants
+  # get "/restaurants/:id/chef", to: "restaurants#chef", as: :restaurant_chef
 end
+
+
+# link_to "top restos", top_restaurants_path
+# link_to "chef", restaurant_chef_path
